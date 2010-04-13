5535c7f538dce0804dffd134007e7f5312c07edf
Masanobu Imai <masanobu.imai@gmail.com>
2010-04-14 00:12:41 +0900

    DnDのサンプルをインライン展開してコンパクトにした。

--- dnd.groovy
+++ dnd.groovy
@@ -9,33 +9,27 @@ import java.awt.dnd.*
 
 def myList
 
-def dropListener = [
-  drop: {e ->
-    e.acceptDrop(ACTION_COPY_OR_MOVE)
-    myList.model.removeAllElements()
-    try {
-      if (e.isDataFlavorSupported(javaFileListFlavor)) {
-        def trans = e.transferable
-        trans.getTransferData(javaFileListFlavor).each {
-          myList.model.addElement(it)
-        }
-      }
-    } catch (ex) {
-      e.dropComplete(false)
-    }
-  },
-  dragEnter: {e ->
-    e.acceptDrag(ACTION_COPY_OR_MOVE)
-  }
-] as DropTargetAdapter
-
 SwingBuilder.build {
   frame(title: "DnD", pack: true, visible: true,
         defaultCloseOperation: EXIT_ON_CLOSE) {
     scrollPane() {
       myList = list(model: new DefaultListModel(),
-                    dropTarget: new DropTarget(myList, ACTION_COPY_OR_MOVE,
-                                               dropListener, true))
+                    dropTarget: new DropTarget(myList, ACTION_COPY_OR_MOVE, [
+                                               drop: {e ->
+                                                 e.acceptDrop(ACTION_COPY_OR_MOVE)
+                                                 myList.model.removeAllElements()
+                                                 try {
+                                                   if (e.isDataFlavorSupported(javaFileListFlavor)) {
+                                                     def trans = e.transferable
+                                                     trans.getTransferData(javaFileListFlavor).each {
+                                                       myList.model.addElement(it)
+                                                     }
+                                                   }
+                                                 } catch (ex) { e.dropComplete(false) }
+                                               },
+                                               dragEnter: {e ->
+                                                 e.acceptDrag(ACTION_COPY_OR_MOVE)
+                                               } ] as DropTargetAdapter, true))
     }
   }
 }
